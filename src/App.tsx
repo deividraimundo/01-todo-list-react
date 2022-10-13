b08aa228f39740f457c874d542f6d032008481cf
Deivid de Oliveira Raimundo <deivid.raimundo@aswti.com.br>
2022-10-13 17:54:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header'
+
+import styles from './App.module.css'
+import './global.css'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+    </div>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import rocket from '../assets/rocket.svg'
+
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={rocket} alt="Rocket" />
+        <p>to<span>do</span></p>
+      </div>
+    </header>
+  )
+}
